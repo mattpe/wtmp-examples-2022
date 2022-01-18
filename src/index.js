147d83681468a2fa0a295625b08a7cc7b5edc376
mattpe <mattpe@metropolia.fi>
2022-01-18 13:06:20 +0200

    start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,1 @@
-import { myFunction } from "./modules/example-module";
 
-console.log(myFunction('moro'));
--- src/modules/example-module.js
+++
@@ -1,9 +0,0 @@
-const myVariable = 'Hello';
-const myFunction = (name) => {
-  return 'Hello ' + name;
-};
-const myObject = {
-  prop1: 'Prop1 value',
-  prop2: ['Array', 'of', 'prop2', 'values']
-};
-export {myVariable, myFunction, myObject};
