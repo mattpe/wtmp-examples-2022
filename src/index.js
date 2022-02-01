b86c6b0b5b8106757df1d2143a8866c7a93184ce
mattpe <mattpe@metropolia.fi>
2022-02-01 08:34:25 +0200

    w4 lecture examples added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-import { myFunction } from "./modules/example-module";
+// wtmp js examples 31.1.2022
 
-console.log(myFunction('moro'));
+const netPromise = fetch('https://api.github.com/users/mattpe');
+
+netPromise.then(data => data.json()).then((json) => {
+  console.log(json);
+  fetch(json.repos_url).then(data => data.json()).then(data => {
+    console.log(data);
+        // fetch(data[0].collaborators_url).then();
+  });
+}).catch(error => {
+  // do something with the error..
+});
+
+console.log('promise 1', netPromise);
+console.log('moro');
+
+// Async - await & error handling
+const getGithubUserProfile = async username => {
+  let userData = {};
+  try {
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    if (!response.ok) {
+      throw new Error('problem: '+ response.statusText);
+    }
+    userData = await response.json();
+  } catch (error) {
+    console.error(error);
+    // In real case you should nofify user too?
+  }
+  return userData;
+};
+
+getGithubUserProfile('mattpe').then(data => {
+  console.log(data);
+});
